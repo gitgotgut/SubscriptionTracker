5b57817d83e8597f236405423df2900c27444129
Carl-Oscar <carl-oscar.wm@outlook.com>
2026-02-25 19:40:59 +0100

    fix: make /gmail page publicly accessible

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
-const PUBLIC_PATHS = new Set(["/", "/login", "/register"]);
+const PUBLIC_PATHS = new Set(["/", "/login", "/register", "/gmail"]);
 
 export default auth((req) => {
   const { pathname } = req.nextUrl;
